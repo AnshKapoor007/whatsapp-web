5922602945cbb1a671e78f2eb425b27126bbb1b6
AnshKapoor007 <kapoor.ansh28@gmail.com>
2023-11-27 16:43:43 +0530

    qr issue resolved

--- index.js
+++ index.js
@@ -11,36 +11,49 @@ const port = config.application.port || 3000;
 
 app.use(express.json());
 
-async function initializeClient(phoneNumber) {
-    try {
-        await mongoose.connect(config.dbCredentials.dbUrl);
-        const store = new MongoStore({ mongoose: mongoose });
+mongoose.connect(config.dbCredentials.dbUrl)
+    .then(() => {
+        console.log('Connected to the DataBase');
+        startServer();
+    })
+    .catch((err) => {
+        console.error('Error connecting to the database:', err);
+    });
 
-        const client = new Client({
-            authStrategy: new RemoteAuth({
-                clientId: phoneNumber,
-                store: store,
-                backupSyncIntervalMs: 300000
-            })
-        });
+function startServer() {
+    const store = new MongoStore({ mongoose: mongoose });
 
-        app.locals[phoneNumber] = { client };
+    app.locals.clients = {};
 
-        client.initialize();
+    async function initializeClient(phoneNumber) {
+        try {
+            const prevClient = app.locals.clients[phoneNumber];
 
-        return client;
-    } catch (error) {
-        throw error;
+            if (!prevClient) {
+                const client = new Client({
+                    authStrategy: new RemoteAuth({
+                        clientId: phoneNumber,
+                        store: store,
+                        backupSyncIntervalMs: 300000
+                    })
+                });
+
+                app.locals.clients[phoneNumber] = client;
+
+                client.initialize();
+
+                return client;
+            }
+            return prevClient;
+        } catch (error) {
+            throw error;
+        }
     }
-}
 
-app.get('/api/initialize-client/:phoneNumber', async (req, res) => {
-    const phoneNumber = req.params.phoneNumber;
+    app.get('/api/initialize-client/:phoneNumber', async (req, res) => {
+        const phoneNumber = req.params.phoneNumber;
 
-    try {
-        const client = app.locals[phoneNumber] && app.locals[phoneNumber].client;
-
-        if (!client) {
+        try {
             const newClient = await initializeClient(phoneNumber);
 
             let responseSent = false;
@@ -58,44 +71,42 @@ app.get('/api/initialize-client/:phoneNumber', async (req, res) => {
                     res.send({ status: 'CLIENT_READY' });
                 }
             });
-        } else {
-            res.send({ status: 'CLIENT_READY' });
+        } catch (error) {
+            res.status(500).send({ error: 'ERROR CHECKING CLIENT STATUS' });
         }
-    } catch (error) {
-        res.status(500).send({ error: 'ERROR CHECKING CLIENT STATUS' });
-    }
-});
+    });
 
-app.post('/api/send-message/:phoneNumber', async (req, res) => {
-    const senderPhoneNumber = req.params.phoneNumber;
-    const { targetPhoneNumber, message } = req.body;
+    app.post('/api/send-message/:phoneNumber', async (req, res) => {
+        const senderPhoneNumber = req.params.phoneNumber;
+        const { targetPhoneNumber, message } = req.body;
 
-    try {
-        const client = app.locals[senderPhoneNumber].client;
+        try {
+            const client = app.locals.clients[senderPhoneNumber];
 
-        if (!client) {
-            return res.status(404).send({ error: 'CLIENT NOT FOUND' });
+            if (!client) {
+                return res.status(404).send({ error: 'CLIENT NOT FOUND' });
+            }
+
+            const sanitized_number = targetPhoneNumber.toString().replace(/[- )(]/g, "");
+
+            const final_number = `91${sanitized_number.substring(sanitized_number.length - 10)}`;
+
+            const number_details = await client.getNumberId(final_number);
+
+            if (number_details) {
+                for (let i = 0; i < 10; i++) {
+                    await client.sendMessage(number_details._serialized, message);
+                }
+                res.status(200).send({ message: 'MESSAGE SENT SUCCESSFULLY' });
+            } else {
+                res.status(200).send({ message: 'UNABLE TO SEND MESSAGE' });
+            }
+        } catch (error) {
+            res.status(500).send({ error: 'ERROR SENDING MESSAGE' });
         }
+    });
 
-        const sanitized_number = targetPhoneNumber.toString().replace(/[- )(]/g, "");
-
-        const final_number = `91${sanitized_number.substring(sanitized_number.length - 10)}`;
-
-        const number_details = await client.getNumberId(final_number);
-
-        if (number_details) {
-            for (let i = 0; i < 10; i++)
-                await client.sendMessage(number_details._serialized, message);
-            res.status(200).send({ message: 'MESSAGE SENT SUCCESSFULLY' });
-        }
-        else {
-            res.status(200).send({ message: 'UNABLE TO SEND MESSAGE' });
-        }
-    } catch (error) {
-        res.status(500).send({ error: 'ERROR SENDING MESSAGE' });
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+    app.listen(port, () => {
+        console.log(`Server is running at http://localhost:${port}`);
+    });
+}
